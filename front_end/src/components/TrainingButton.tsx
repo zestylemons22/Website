cba7177f3cfbe4386604f7046657fba53d5ffeed
Alex Taylor <zestylemons22@gmail.com>
2024-11-15 15:01:37 +1300

    full hiragana page working

--- front_end/src/components/TrainingButton.tsx
+++ front_end/src/components/TrainingButton.tsx
@@ -1,36 +1,37 @@
-import React, { ReactNode, useState } from "react";
+import React, { useState, forwardRef } from "react";
 
 interface Props {
   children: string;
   correctAnswer: string;
   colour?: string;
+  onCorrectAnswer?: () => void;
 }
 
-var TrainingButton = ({
-  children,
-  correctAnswer,
-  colour = "primary",
-}: Props) => {
-  const [Colour, SetColour] = useState(colour);
-  const HandleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      const inputValue = event.currentTarget.value;
-      console.log("enter pressed");
-      if (inputValue === correctAnswer) {
-        SetColour("success");
-      } else {
-        SetColour("danger");
-      }
-    }
-  };
+const TrainingButton = forwardRef<HTMLInputElement, Props>(
+  ({ children, correctAnswer, colour = "primary", onCorrectAnswer }, ref) => {
+    const [Colour, SetColour] = useState(colour);
 
-  return (
-    <button type="button" className={"btn btn-" + Colour}>
-      {children}
-      <br />
-      <input onKeyDown={HandleKeyDown} type="text"></input>
-    </button>
-  );
-};
+    const HandleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === "Enter") {
+        const inputValue = event.currentTarget.value;
+        console.log("enter pressed");
+        if (inputValue === correctAnswer) {
+          SetColour("success");
+          onCorrectAnswer?.();
+        } else {
+          SetColour("danger");
+        }
+      }
+    };
+
+    return (
+      <button type="button" className={"btn btn-" + Colour}>
+        <span className="h-3"> {children}</span>
+        <br />
+        <input ref={ref} onKeyDown={HandleKeyDown} type="text"></input>
+      </button>
+    );
+  }
+);
 
 export default TrainingButton;
--- front_end/src/pages/Hiragana.tsx
+++ front_end/src/pages/Hiragana.tsx
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useRef } from "react";
 import TrainingButton from "../components/TrainingButton";
+import { shuffle } from "lodash";
+
+const Characters = [
+  ["あ", "a"],
+  ["い", "i"],
+  ["う", "u"],
+  ["え", "e"],
+  ["お", "o"],
+  ["か", "ka"],
+  ["き", "ki"],
+  ["く", "ku"],
+  ["け", "ke"],
+  ["こ", "ko"],
+  ["さ", "sa"],
+  ["し", "shi"],
+  ["す", "su"],
+  ["せ", "se"],
+  ["そ", "so"],
+  ["た", "ta"],
+  ["ち", "chi"],
+  ["つ", "tsu"],
+  ["て", "te"],
+  ["と", "to"],
+  ["な", "na"],
+  ["に", "ni"],
+  ["ぬ", "nu"],
+  ["ね", "ne"],
+  ["の", "no"],
+  ["は", "ha"],
+  ["ひ", "hi"],
+  ["ふ", "fu"],
+  ["へ", "he"],
+  ["ほ", "ho"],
+  ["ま", "ma"],
+  ["み", "mi"],
+  ["む", "mu"],
+  ["め", "me"],
+  ["も", "mo"],
+  ["や", "ya"],
+  ["ゆ", "yu"],
+  ["よ", "yo"],
+  ["ら", "ra"],
+  ["り", "ri"],
+  ["る", "ru"],
+  ["れ", "re"],
+  ["ろ", "ro"],
+  ["わ", "wa"],
+  ["を", "wo"],
+  ["ん", "n"],
+];
 
 const Hiragana = () => {
+  const shuffledCharacters = shuffle(Characters);
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleCorrectAnswer = (index: number) => {
+    if (index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
   return (
     <>
-      <div>hello!</div>
-      <TrainingButton correctAnswer="tsu">つ</TrainingButton>
+      <br />
+      <div className="d-flex flex-wrap">
+        {shuffledCharacters.map((char, index) => (
+          <div className="p-2" key={index}>
+            <TrainingButton
+              correctAnswer={char[1]}
+              onCorrectAnswer={() => handleCorrectAnswer(index)}
+              ref={(el) => (inputRefs.current[index] = el)}
+            >
+              {char[0]}
+            </TrainingButton>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
